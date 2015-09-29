9d6be7b14b1ecdd278a35a507404ac7a7c8e3249
Diwas Adhikary <diadical@diadic.net>
2015-09-29 19:28:11 +1000

    Updated Comparator class to have ascending and descending instances as properties rather than a single instance property and restructured tests. Add JSDoc comments throughout file. Removed mocha as a standard environment for all files by updating eslintrc and updated unit test files to turn on mocha mode.

--- src/util/comparators/Comparator.js
+++ src/util/comparators/Comparator.js
@@ -1,37 +1,99 @@
-
+/**
+ * Generic Comparator class for comparing two values.
+ */
 class Comparator {
-  static get instance() {
-    let obj = Comparator.prototype._instance;
-    if (!obj) {
-      obj = new Comparator();
-      Comparator.prototype._instance = obj;
-    }
-    return obj;
+  /**
+   * Static property to get a singleton instance of Comparator class in ascending order.
+   * @member {Comparator}
+   * @readonly
+   */
+  static get ascending() {
+    return ascendingInstance;
   }
 
-  compare(v1, v2) {
-    if (v1 < v2) {
-      return -1;
+  /**
+  * Static property to get a singleton instance of Comparator class in descending order.
+  * @member {Comparator}
+  * @readonly
+  */
+  static get descending() {
+    return descendingInstance;
+  }
+
+  /**
+   * @param {Boolean} [descendingOrder=false] - Order of comparisons. Descending order if true otherwise ascending order.
+   */
+  constructor(descendingOrder) {
+    this._descendingOrder = !!descendingOrder;
+  }
+
+  /**
+   * Returns whether or not comparisons are done in descending or ascending order.
+   * @member {Boolean}
+   * @readonly
+   */
+  get isDescendingOrder() {
+    return this._descendingOrder;
+  }
+
+  /**
+   * Compares two values and returns a comparison result.
+   *
+   * This default Comparator implementation simply uses JavaScripts inbuilt
+   * less than (<) or greather than (>) to compare values, if neither then they
+   * are considered to be the same. This comparison does not consider the types
+   * of the two values passed in.
+   *
+   * What is considered 'less than' or 'greater than' depends on whether a
+   * Comparator instance is configured to work in ascending or descending order.
+   * @param  {} value1 The first value to compare.
+   * @param  {} value2 The second value to compare.
+   * @return {Number} 0 if the values are the same, -1 if value1 is less than value 2 and 1 if value1 is greater than value2.
+   */
+  compare(value1, value2) {
+    if (value1 < value2) {
+      return this._descendingOrder ? 1 : -1;
     }
-    else if (v1 > v2) {
-      return 1;
+    else if (value1 > value2) {
+      return this._descendingOrder ? -1 : 1;
     }
     else {
       return 0;
     }
   }
 
-  equals(v1, v2) {
-    return this.compare(v1, v2) === 0;
+  /**
+   * Compares two values and returns true if value1 equals value2.
+   * @param  {} value1 The first value to compare.
+   * @param  {} value2 The second value to compare.
+   * @return {Boolean} true if value1 equals value2, otherwise false.
+   */
+  equals(value1, value2) {
+    return this.compare(value1, value2) === 0;
   }
 
-  lessThan(v1, v2) {
-    return this.compare(v1, v2) === -1;
+  /**
+   * Compares two values and returns true if value1 is less than value2.
+   * @param  {} value1 The first value to compare.
+   * @param  {} value2 The second value to compare.
+   * @return {Boolean} true if value1 is less than value2, otherwise false.
+   */
+  lessThan(value1, value2) {
+    return this.compare(value1, value2) === -1;
   }
 
-  greaterThan(v1, v2) {
-    return this.compare(v1, v2) === 1;
+  /**
+   * Compares two values and returns true if value1 is greater than value2.
+   * @param  {} value1 The first value to compare.
+   * @param  {} value2 The second value to compare.
+   * @return {Boolean} true if value1 is greater than value2, otherwise false.
+   */
+  greaterThan(value1, value2) {
+    return this.compare(value1, value2) === 1;
   }
 }
+
+var ascendingInstance = new Comparator(false),
+  descendingInstance = new Comparator(true);
 
 export default Comparator;
--- src/util/comparators/Comparator.spec.js
+++ src/util/comparators/Comparator.spec.js
@@ -1,3 +1,5 @@
+/* eslint-env mocha */
+
 import { expect } from 'chai';
 import Comparator from './Comparator';
 
@@ -5,102 +7,118 @@ describe('Comparator class', function() {
 
   describe('instantiation', function() {
 
-    it('can be instantiated with  no arguments', function() {
-      let obj = new Comparator();
+    it('can be instantiated with no arguments', function() {
+      expect(() => new Comparator()).to.not.throw(Error);
+    });
 
-      expect(obj.compare).to.not.be.undefined;
-      expect(obj.equals).to.not.be.undefined;
-      expect(obj.lessThan).to.not.be.undefined;
-      expect(obj.greaterThan).to.not.be.undefined;
+    it('can be instantiated with descendingOrder = false', function() {
+      expect(() => new Comparator(false)).to.not.throw(Error);
+    });
+
+    it('can be instantiated with descendingOrder = true', function() {
+      expect(() => new Comparator(true)).to.not.throw(Error);
     });
 
   });
 
-  describe('.instance', function() {
+  [
+    {
+      prop: 'ascending',
+      multiplier: 1
+    },
+    {
+      prop: 'descending',
+      multiplier: -1
+    }
+  ].forEach(context => {
+    describe('#' + context.prop, function() {
 
-    it('returns an instance of Comparator', function() {
-      let obj = Comparator.instance;
-      expect(obj).to.not.be.undefined;
-      expect(obj instanceof Comparator).to.be.true;
-    });
+      beforeEach(function() {
+        this.obj = Comparator[context.prop];
+      });
 
-    it('always returns the same object', function() {
-      for (let i = 0; i < 10; ++i) {
-        expect(Comparator.instance).to.equal(Comparator.instance);
-      }
-    });
+      it('returns an instance of Comparator', function() {
+        expect(Comparator).to.have.property(context.prop);
+        expect(this.obj).to.be.an.instanceof(Comparator);
+      });
 
-  });
+      it('always returns the same object', function() {
+        expect(this.obj).to.equal(Comparator[context.prop]);
+      });
 
-  describe('.compare', function() {
+      describe('#compare', function() {
 
-    it('returns 0 when both numbers are the same', function() {
-      for (let i = -200; i <= 200; i += 17) {
-        expect(Comparator.instance.compare(i, i)).to.equal(0);
-      }
-    });
+        it('returns 0 when both numbers are the same', function() {
+          for (let i = -200; i <= 200; i += 17) {
+            expect(this.obj.compare(i, i)).to.equal(0);
+          }
+        });
 
-    it('returns -1 when first number is smaller than second', function() {
-      for (let i = -200; i <= 200; i += 17) {
-        expect(Comparator.instance.compare(i, i + 7)).to.equal(-1);
-      }
-    });
+        it('returns -1 when first number is less than the second', function() {
+          for (let i = -200; i <= 200; i += 17) {
+            expect(this.obj.compare(i, i + 7 * context.multiplier)).to.equal(-1);
+          }
+        });
 
-    it('returns 1 when first number is larger than second', function() {
-      for (let i = -200; i <= 200; i += 17) {
-        expect(Comparator.instance.compare(i, i - 7)).to.equal(1);
-      }
-    });
+        it('returns 1 when first number is greater than the second', function() {
+          for (let i = -200; i <= 200; i += 17) {
+            expect(this.obj.compare(i, i - 7 * context.multiplier)).to.equal(1);
+          }
+        });
 
-  });
+      });
 
-  describe('.equals', function() {
+      describe('#equals', function() {
 
-    it('returns true when both numbers are the same', function() {
-      for (let i = -200; i <= 200; i += 17) {
-        expect(Comparator.instance.equals(i, i)).to.be.true;
-      }
-    });
+        it('returns true when both numbers are the same', function() {
+          for (let i = -200; i <= 200; i += 17) {
+            expect(this.obj.equals(i, i)).to.be.true;
+          }
+        });
 
-    it('returns false when both numbers are not the same', function() {
-      for (let i = -200; i <= 200; i += 17) {
-        expect(Comparator.instance.equals(i, i - 7)).to.be.false;
-        expect(Comparator.instance.equals(i, i + 7)).to.be.false;
-      }
-    });
+        it('returns false when both numbers are not the same', function() {
+          for (let i = -200; i <= 200; i += 17) {
+            expect(this.obj.equals(i, i - 7)).to.be.false;
+            expect(this.obj.equals(i, i + 7)).to.be.false;
+          }
+        });
 
-  });
+      });
 
-  describe('.lessThan', function() {
+      describe('#lessThan', function() {
 
-    it('returns true when first number is smaller than second', function() {
-      for (let i = -200; i <= 200; i += 17) {
-        expect(Comparator.instance.lessThan(i, i + 7)).to.be.true;
-      }
-    });
+        it('returns true when first number is less than the second', function() {
+          for (let i = -200; i <= 200; i += 17) {
+            expect(this.obj.lessThan(i, i + 7 * context.multiplier)).to.be.true;
+          }
+        });
 
-    it('returns false when first number is larger or equal to the second', function() {
-      for (let i = -200; i <= 200; i += 17) {
-        expect(Comparator.instance.lessThan(i, i)).to.be.false;
-        expect(Comparator.instance.lessThan(i, i - 7)).to.be.false;
-      }
-    });
+        it('returns false when first number is greater or equal to the second', function() {
+          for (let i = -200; i <= 200; i += 17) {
+            expect(this.obj.lessThan(i, i)).to.be.false;
+            expect(this.obj.lessThan(i, i - 7 * context.multiplier)).to.be.false;
+          }
+        });
 
-  });
+      });
 
-  describe('.greaterThan', function() {
+      describe('#greaterThan', function() {
 
-    it('returns true when first number is larger than second', function() {
-      for (let i = -200; i <= 200; i += 17) {
-        expect(Comparator.instance.greaterThan(i, i - 7)).to.be.true;
-      }
-    });
+        it('returns true when first number is larger than second', function() {
+          for (let i = -200; i <= 200; i += 17) {
+            expect(this.obj.greaterThan(i, i - 7 * context.multiplier)).to.be.true;
+          }
+        });
 
-    it('returns false when first number is smaller or equal to the second', function() {
-      for (let i = -200; i <= 200; i += 17) {
-        expect(Comparator.instance.greaterThan(i, i)).to.be.false;
-        expect(Comparator.instance.greaterThan(i, i + 7)).to.be.false;
-      }
+        it('returns false when first number is smaller or equal to the second', function() {
+          for (let i = -200; i <= 200; i += 17) {
+            expect(this.obj.greaterThan(i, i)).to.be.false;
+            expect(this.obj.greaterThan(i, i + 7 * context.multiplier)).to.be.false;
+          }
+        });
+
+      });
+
     });
 
   });
