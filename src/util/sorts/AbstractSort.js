a234a02b9439c098b44ea7e287101fbb1d1f73ea
Diwas Adhikary <diadical@diadic.net>
2015-11-08 14:12:15 +1000

    Added JSDoc comments to AbstractSort class.

--- src/util/sorts/AbstractSort.js
+++ src/util/sorts/AbstractSort.js
@@ -1,6 +1,18 @@
 import Comparator from '../comparators/Comparator';
 
+/**
+ * Abstract sorting class that contains utility methods and singleton instanceof
+ * method implementations. Cannot be instantiated by itself.
+ */
 class AbstractSort {
+  /**
+   * Returns a singleton instance of a sorting subclass that has been supplied
+   * Comparator in ascending sort order. Should only be called on subclasses
+   * of this AbstractSort class.
+   *
+   * @member {AbstractSort}
+   * @readonly
+   */
   static get ascending() {
     if (!this.hasOwnProperty('_ascending')) {
       this._ascending = new this(Comparator.ascending);
@@ -8,6 +20,14 @@ class AbstractSort {
     return this._ascending;
   }
 
+  /**
+   * Returns a singleton instance of a sorting subclass that has been supplied
+   * Comparator in descending sort order. Should only be called on subclasses
+   * of this AbstractSort class.
+   *
+   * @member {AbstractSort}
+   * @readonly
+   */
   static get descending() {
     if (!this.hasOwnProperty('_descending')) {
       this._descending = new this(Comparator.descending);
@@ -15,6 +35,12 @@ class AbstractSort {
     return this._descending;
   }
 
+  /**
+   * Constructor for AbstractSort cannot be invoked directly, should only be
+   * called by a subclass via super() in a subclass constructor.
+   *
+   * @param  {Comparator} comparator The Comparator object to compare values with
+   */
   constructor(comparator) {
     if (this.constructor === AbstractSort) {
       throw new TypeError('AbstractSort: cannot instantiate abstract class');
@@ -26,10 +52,23 @@ class AbstractSort {
     this._comparator = comparator;
   }
 
+  /**
+   * Returns the comparator object that this Sort object was created with.
+   *
+   * @member {Comparator}
+   * @readonly
+   */
   get comparator() {
     return this._comparator;
   }
 
+  /**
+   * Abstract method that runs the sort routine. This method itself cannot be
+   * called and will simply throw an error. It is expected to be overloaded
+   * by any implementing subclass.
+   *
+   * @throws {Error} Will always throw an Error
+   */
   sort() { // (comparator, array)
     // must be implemented in subclasses
     throw new Error('sort is not implemented!');
